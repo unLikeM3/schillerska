3b42ac067b250a25304c7145d64e7dd0d8e4440b
root <root@unlikeme.(none)>
2013-06-25 04:00:47 +0200

    Animated loading

--- tillsvidare/js/functions.js
+++ tillsvidare/js/functions.js
@@ -1,8 +1,10 @@
 $(document).ready(function() {
+	$('#wrapper').fadeIn(1200);
+
 	var toggle = false;
 	$('#kontaktbtn').click(function() {
 		if(toggle == false){
-			$('#kontaktinfo').slideDown(1500);
+			$('#kontaktinfo').slideDown(1000);
 			toggle = true;
 		}else{
 			$('#kontaktinfo').slideUp(500);
