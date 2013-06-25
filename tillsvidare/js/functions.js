b559fb9ee6e007a8f213d13b293ca572402e0e94
root <root@unlikeme.(none)>
2013-06-25 02:22:26 +0200

    Added javascript functions

--- tillsvidare/js/functions.js
+++ tillsvidare/js/functions.js
@@ -1,30 +1,5 @@
 $(document).ready(function() {
-	showPage('homez');
-
-
-
-	$('.aboutpage').click(function() {
-		showPage('about');
-		$('.active').removeClass('active');
-		$('.aboutpage').addClass('active');
-	});
-	$('.homepage').click(function() {
-		showPage('homez');
-		$('.active').removeClass('active');
-		$('.homepage').addClass('active');
-	});
-	$('.contactpage').click(function() {
-		showPage('contact');
-		$('.active').removeClass('active');
-		$('.contactpage').addClass('active');
+	$('#kontaktbtn').click(function() {
+		$('#kontaktinfo').slideDown(1500);
 	});
 });
-
-function showPage (page) {
-	$('#mc').fadeOut(250, function() {
-		$.get(page+'.php', function(data) {
-			$('#mc').html(data);
-			$('#mc').fadeIn(250);
-		});
-	});
-}
