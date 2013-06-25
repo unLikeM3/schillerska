4dd0c20a37dc7efdc9a79bb28a9e73255288a758
root <root@unlikeme.(none)>
2013-06-25 04:53:10 +0200

    Fixed background. Hopefully.

--- tillsvidare/js/functions.js
+++ tillsvidare/js/functions.js
@@ -1,4 +1,25 @@
 $(document).ready(function() {
+	/*var theWindow        = $(window),
+	$bg              = $("#bg"),
+	aspectRatio      = $bg.width() / $bg.height();
+	    			    		
+	function resizeBg() {
+		
+		if ( (theWindow.width() / theWindow.height()) < aspectRatio ) {
+		    $bg
+		    	.removeClass()
+		    	.addClass('bgheight');
+		} else {
+		    $bg
+		    	.removeClass()
+		    	.addClass('bgwidth');
+		}
+					
+	}
+	                   			
+	theWindow.resize(resizeBg).trigger("resize");*/
+	
+
 	$('#wrapper').fadeIn(1200);
 
 	var toggle = false;
@@ -13,3 +34,4 @@ $(document).ready(function() {
 		
 	});
 });
+
