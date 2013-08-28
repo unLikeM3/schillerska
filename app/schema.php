70c6ae671704dc65a01009c19ebc811186703c21
John Eriksson <johnreriksson@gmail.com>
2013-08-28 02:56:45 +0200

    Added 'dag' to schedule

--- app/schema.php
+++ app/schema.php
@@ -1,8 +1,17 @@
 <?php 
 	header("Access-Control-Allow-Origin: *");
 ?>
+<div class="ui-block-solo"><input type="text" id="pnr" placeholder="YYMMDDXXXX"> </div>
 <div class="ui-grid-a">
-	<div class="ui-block-a"><input data-inline="true" type="text" id="pnr" placeholder="YYMMDDXXXX"> </div>
+	<div class="ui-block-a">
+		<select id="day">
+			<option value="1">Måndag</option>
+			<option value="2">Tisdag</option>
+			<option value="4">Onsdag</option>
+			<option value="8">Torsdag</option>
+			<option value="16">Fredag</option>
+		</select>
+	</div>
 	<div class="ui-block-b">
 		<select id="period">
 	    	<option value="HT">HT</option>
