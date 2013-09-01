dc542eec7cb3bc6abb750c1843dcc212633913b6
John Eriksson <yanis.cool3@gmail.com>
2013-09-01 13:20:18 +0200

    Working with infscroll on news, broken atm

--- Backend/loadposts.php
+++ Backend/loadposts.php
@@ -2,25 +2,23 @@
 	require 'connect.php';
 	header("Access-Control-Allow-Origin: *");
 
-	//$offset = is_numeric($_POST['offset']) ? $_POST['offset'] : die();
-    //$postnumbers = is_numeric($_POST['number']) ? $_POST['number'] : die();
+	$offset = is_numeric($_POST['offset']) ? $_POST['offset'] : die();
+    $postnumbers = is_numeric($_POST['number']) ? $_POST['number'] : die();
 
-	$query2 = mysql_query("SELECT * FROM _posts WHERE post_status='publish' AND post_type='post' ORDER BY post_date DESC"); //LIMIT ".$postnumbers." OFFSET ".$offset);
-	
-	while ($row2 = mysql_fetch_assoc($query2)) { ?>
+	$query = mysql_query("SELECT * FROM _posts WHERE post_status='publish' AND post_type='post' ORDER BY post_date DESC LIMIT ".$postnumbers." OFFSET ".$offset);
+	while ($row = mysql_fetch_assoc($query)) { ?>
 
+		<div>
 		<div class="inlagg">
 			<div class="wrapper">    
 				<div class="title">
-					<h2><?php echo $row2['post_title']; ?></h2>
+					<h2><?php echo $row['post_title']; ?></h2>
 				</div>
 
 				<div class="content">
-					<?php echo $row2['post_content'];?>
-				</div>
-				<div class="meta">
-
+					<?php echo $row['post_content'];?>
 				</div>
 			</div>
+		</div>
 		</div>
 	<?php } ?>
