5616881ad744114fa64dbfe9abc292d5748c2831
John Eriksson <johnreriksson@gmail.com>
2013-11-11 00:57:13 +0100

    fixed iframe bug

--- Backend/loadposts.php
+++ Backend/loadposts.php
@@ -6,17 +6,23 @@
     $postnumbers = is_numeric($_POST['number']) ? $_POST['number'] : die();
 
 	$query = mysql_query("SELECT * FROM _posts WHERE post_status='publish' AND post_type='post' ORDER BY post_date DESC LIMIT ".$postnumbers." OFFSET ".$offset);
-	while ($row = mysql_fetch_assoc($query)) { ?>
+	while ($row = mysql_fetch_assoc($query)) { 
+		$title = $row['post_title'];
+		$content = $row['post_content'];
 
-		<p>
+		$content = str_ireplace('//i', 'http://i', $content);
+		$content = str_ireplace('//www.y', 'http://www.y', $content);
+
+
+		?>
 			<div class="inlagg">
-				<div class="inlaggwrapper wrapper">    
+				<div class="wrapper">    
 					<div class="title">
-						<h2><?php echo $row['post_title']; ?></h2>
+						<h2><?php echo $title; ?></h2>
 					</div>
 
-					<div class="inlaggcontent">
-						<?php echo $row['post_content'];?>
+					<div class="content">
+						<?php echo $content; ?>
 					</div>
 
 					<div class="meta">
@@ -24,5 +30,7 @@
 					</div>
 				</div>
 			</div>
-		</p>
+
+
+			
 	<?php } ?>
