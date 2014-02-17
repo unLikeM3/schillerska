4ede91695e834045f37dbc3c95040073fa81a0d5
john.e <johnreriksson@gmail.com>
2014-02-17 01:20:55 +0100

    changed id of app

--- Backend/loadposts.php
+++ Backend/loadposts.php
@@ -1,12 +1,23 @@
 <?php 
-	require 'connect.php';
+	require 'connect_local.php';
 	header("Access-Control-Allow-Origin: *");
 
-	$offset = is_numeric($_POST['offset']) ? $_POST['offset'] : die();
-    $postnumbers = is_numeric($_POST['number']) ? $_POST['number'] : die();
+	$offset = is_numeric($_POST['offset']) ? $_POST['offset'] : 0;
+    $postnumbers = is_numeric($_POST['number']) ? $_POST['number'] : 10;
 
 	$query = mysql_query("SELECT * FROM _posts WHERE post_status='publish' AND post_type='post' ORDER BY post_date DESC LIMIT ".$postnumbers." OFFSET ".$offset);
-	while ($row = mysql_fetch_assoc($query)) { 
+	
+	$posts = array();
+
+	while($row = mysql_fetch_object($query)){
+		$post = array('title' => $row->post_title, 'content' => $row->post_content);
+		$posts[] = $post;
+	}
+
+	var_dump($posts);
+
+
+	/*while ($row = mysql_fetch_assoc($query)) { 
 		$title = $row['post_title'];
 		$content = $row['post_content'];
 
@@ -33,4 +44,4 @@
 
 
 			
-	<?php } ?>
+	<?php }*/ ?>
